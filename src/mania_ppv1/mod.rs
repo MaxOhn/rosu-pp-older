996c069768ad212a474531ae9fb7928c2c5491f2
MaxOhn <ohn.m@hotmail.de>
2022-07-07 15:17:27 +0200

    applied rosu-pp update

--- src/mania_ppv1/mod.rs
+++ src/mania_ppv1/mod.rs
@@ -4,7 +4,7 @@ mod strain;
 pub use pp::*;
 use strain::Strain;
 
-use rosu_pp::{mania::ManiaDifficultyAttributes, parse::HitObject, Beatmap, GameMode, Mods};
+use rosu_pp::{mania::ManiaDifficultyAttributes, parse::HitObject, Beatmap, Mods};
 
 const SECTION_LEN: f32 = 400.0;
 const STAR_SCALING_FACTOR: f32 = 0.018;
@@ -23,28 +23,7 @@ pub fn stars(
         return ManiaDifficultyAttributes::default();
     }
 
-    let rounded_cs = map.cs.round();
-
-    let columns = match map.mode {
-        GameMode::MNA => rounded_cs.max(1.0) as u8,
-        GameMode::STD => {
-            let rounded_od = map.od.round();
-
-            let n_objects = map.n_circles + map.n_sliders + map.n_spinners;
-            let slider_or_spinner_ratio = (n_objects - map.n_circles) as f32 / n_objects as f32;
-
-            if slider_or_spinner_ratio < 0.2 {
-                7
-            } else if slider_or_spinner_ratio < 0.3 || rounded_cs >= 5.0 {
-                6 + (rounded_od > 5.0) as u8
-            } else if slider_or_spinner_ratio > 0.6 {
-                4 + (rounded_od > 4.0) as u8
-            } else {
-                (rounded_od as u8 + 1).max(4).min(7)
-            }
-        }
-        other => panic!("can not calculate mania difficulty on a {:?} map", other),
-    };
+    let columns = map.cs.round().max(1.0) as u8;
 
     let clock_rate = mods.clock_rate() as f32;
     let section_len = SECTION_LEN * clock_rate;
